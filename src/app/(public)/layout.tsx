7ae2e93d84b06216cbdcc54bbc458fe71c16e8d6
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-10-06 00:48:31 +0600

    banner-added

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,9 +1,11 @@
+import Banner from "@/components/Banner";
 import Navbar from "@/components/Navbar";
 import { ChildrenProps } from "@/types/children";
 
 export default function PublicLayout({ children }: ChildrenProps) {
   return (
     <>
+      <Banner />
       <Navbar />
       {children}
     </>
